625d403ae49143f579264a3ea9f6bbd19bbb81a9
Joanne Yeung <jttyeung@gmail.com>
2017-07-26 14:35:41 -0700

    updated env port

--- site/app.js
+++ site/app.js
@@ -5,11 +5,17 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
 
+
+// Routes
 app.get('/', function(req, res) {
     res.render('index');
 })
 
 
-app.listen(3000, function() {
+
+// Port for AWS deployment or local
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
   console.log('App launched on port 3000.')
 })
