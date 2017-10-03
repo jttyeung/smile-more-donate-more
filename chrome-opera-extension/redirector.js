8397edba5ec2826c5cfc02c14c90d8f1785798de
Joanne Yeung <jttyeung@gmail.com>
2017-10-02 17:42:51 -0700

    addresses #5 for fix #6

--- chrome-opera-extension/redirector.js
+++ chrome-opera-extension/redirector.js
@@ -4,28 +4,22 @@ chrome.webRequest.onBeforeRequest.addListener(
 
   function(details) {
     var url = details.url;
-
+    var signIn = new RegExp(/ap\/signin\?_encoding/);
     // Amazon URLs to ignore when redirecting
     var filters = [ '(redirect=true)',
                     '(redirector.html)',
-                    '(/ap/)',
                     '(smdm-noredirect=true)',
                     ];
+
+    if(url.match(signIn)) {
+      url = removesExistingRedirectRule(url);
+      return { redirectUrl: url };
+    }
 
     // Fixes too many redirects bug when
     // user is not logged to Amazon
     if(url.match(filters.join('|'))) {
       return;
-    }
-
-    // If user is redirected, e.g. to the Amazon login page, the
-    // smdm-noredirect rule is removed from the existing URL before
-    // reconstructing AmazonSmile URL. Prevents subsequent pages from
-    // being exempt from Smile More, Donate More URL redirects.
-    var existingRedirect = new RegExp(/'smdm-noredirect%3Dtrue'/)
-
-    if(url.match(existingRedirect)) {
-      url = url.split(existingRedirect).join()
     }
 
     // Returns AmazonSmile URL
@@ -45,12 +39,24 @@ chrome.webRequest.onBeforeRequest.addListener(
 );
 
 
-// Constructs an AmazonSmile URL given an existing Amazon URL
-// Redirects request to AmazonSmile
+function removesExistingRedirectRule(url){
+    // If user is redirected, e.g. to the Amazon login page, the
+    // smdm-noredirect rule is removed from the existing URL before
+    // reconstructing AmazonSmile URL. Prevents subsequent pages from
+    // being exempt from Smile More, Donate More URL redirects.
+    var existingRedirect = new RegExp(/smdm-noredirect%3Dtrue/);
+
+    return url.split(existingRedirect).join();
+}
+
+
 function smileUrlConstructor(url) {
+  // Constructs an AmazonSmile URL given an existing Amazon URL
+  // Redirects request to AmazonSmile
   var amazonSmile = 'https://smile.amazon.com';
   var regexAmazon = new RegExp(/(amazon\.com)/);
   var parseAmazonUrl = url.split(regexAmazon);
+
   var amazonProduct = parseAmazonUrl[parseAmazonUrl.length-1];
 
   var regexQueryString = new RegExp(/(\?)/);
@@ -63,13 +69,15 @@ function smileUrlConstructor(url) {
   } else {
     return { redirectUrl: decodedUrl + '?' + smileMoreNoRedirect };
   }
-
 }
 
 
-// Decodes URI if necessary (e.g. encoded as a result of a redirect)
-function uriDecoder(url) {
-  if(url.indexOf('%') != -1) {
-    return decodeURIComponent(url);
+function uriDecoder(smileUrl) {
+// Decodes URL strings if referred from non-Amazon site
+  var existingRedirect = new RegExp(/smdm-noredirect%3Dtrue/);
+
+  if(smileUrl.indexOf('%') != -1) {
+    return decodeURIComponent(smileUrl);
   }
+  return smileUrl;
 }
